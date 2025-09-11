c329415a11f8ac089e0bf54f5902e1839f586198
AvishekWithTheV <73641829+notDeadCaller@users.noreply.github.com>
2025-09-11 21:11:11 +0530

    Update server.c
    Added Golden Key implementation
    Added color to console message display
    Added sound notification upon message receipt
    Minor formatting improvements

--- server.c
+++ server.c
@@ -1,4 +1,4 @@
-//TCP Echo Server Program
+//TCP Client Server Program
 
 #include<sys/socket.h>
 #include<sys/types.h>
@@ -10,18 +10,19 @@
 #include<unistd.h>
 
 #define PSSWRD "theyarewatching123"
+#define GOLDENKEY "DontForgetTheGoldenKey123$"
 #define MAX_ALLOWED 20   //max IPs in whiteleist
 char *allowed_ips[MAX_ALLOWED];//string to store whitelist client IPs
 int allowed_count=0;
+int goldenKeyFlag=0;
 
 struct sockaddr_in serv_addr, cli_addr;
 
 int listenfd, connfd, r, w,val, cli_addr_len;
 const int sizeFail=5, sizeOk=3;
 
-unsigned short serv_port=25021;
+unsigned short serv_port=25020;
 char serv_ip[]="0.0.0.0";
-
 char buff[128];	//stores received strings
 char rbuff[128];
 
@@ -29,7 +30,7 @@ void getAllowlist(const char *fP);
 int isAllowed(const char *clientIP);
 
 int main() {
-
+	
 	printf("CHAT SERVER\n");
 
 	if((listenfd=socket(AF_INET, SOCK_STREAM, 0))<0)
@@ -37,14 +38,14 @@ int main() {
 		printf("\nserver error: cannot create socket!\n");
 		exit(1);
 	}
-
+	
 	bzero(&serv_addr, sizeof(serv_addr));
 
 	serv_addr.sin_family=AF_INET;
 	serv_addr.sin_port=htons(serv_port);
 	inet_aton(serv_ip, (&serv_addr.sin_addr));
 
-
+	
 	if((bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)))<0)
 	{
 		printf("\nSERVER ERROR: cannot bind");
@@ -52,15 +53,14 @@ int main() {
 		exit(1);
 	}
 
-	if((listen(listenfd, 5))<0)
+	if((listen(listenfd, 5))<0) 
 	{
 		printf("\nSERVER ERROR cannot listen");
 		close(listenfd);
 		exit(1);
 	}
-
-	getAllowlist("firewall.conf");
-
+	
+	getAllowlist("firewall.conf");	
 
 	cli_addr_len=sizeof(cli_addr);
 	for(; ;)
@@ -68,64 +68,73 @@ int main() {
 		printf("SERVER Listening for clients\n");
 		if((connfd=accept(listenfd, (struct sockaddr*)&cli_addr,&cli_addr_len))<0)
 		{
-			printf("SERVER ERROR cannot accept client coonections");
+			printf("SERVER ERROR cannot accept client coonections\n");
 			close(listenfd);
 			exit(1);
 		}
 		char client_ip[INET_ADDRSTRLEN];
 		inet_ntop(AF_INET, &cli_addr.sin_addr, client_ip, sizeof(client_ip));
-
-		if (!isAllowed(client_ip)) {
-		    printf("Connection from %s REJECTED (not in allowlist)\n", client_ip);
-		    close(connfd);
-		    continue;
-		}
-
-		printf("SERVER connection from client %s accepted\n",inet_ntoa(cli_addr.sin_addr));
-
-	//Initiate Password Auth
-		r=read(connfd,buff,128);
+		
+		printf("SERVER connection from client %s received\n",inet_ntoa(cli_addr.sin_addr));
+		
+	//Initiate Password Auth	
+		r=read(connfd,buff,128);	
 		if(r<0) {
     			printf("SERVER Error: no password received\n");
     			close(connfd);
     			continue;
 		}
 		buff[r]='\0';
-		if(strcmp(buff,PSSWRD)!=0) {
-			printf("SERVER: Wrong password from client %s\n",inet_ntoa(cli_addr.sin_addr));
-			write(connfd,"FAIL",sizeFail);	//TODO: put len of FAIL (5) in const int for safety
-			close(connfd);
-			continue;
-		} else {
-			write(connfd,"OK",sizeOk);	//TODO: put len of OK (3) in const int for safety
-			printf("SERVER: Auth success for client %s\n",inet_ntoa(cli_addr.sin_addr));
+		if(strcmp(buff,GOLDENKEY)==0) {//if golden access key entered, skip authentication
+			goldenKeyFlag=1;
+			printf("***Golden Key ACTIVATED***\n");
 		}
-
+			
+		if(goldenKeyFlag!=1) {
+			
+			if(strcmp(buff,PSSWRD)!=0) {
+				printf("SERVER: Wrong password from client %s\n",inet_ntoa(cli_addr.sin_addr));
+				write(connfd,"FAIL",sizeFail);	
+				close(connfd);
+				continue;
+			} else if (!isAllowed(client_ip)) { //if not allowlisted,
+			    printf("\a\033[0;31mConnection from %s REJECTED (not in allowlist)\033[0m\n", client_ip);
+			    close(connfd);
+			    continue;		
+			}
+		}
+			write(connfd,"OK",sizeOk);	
+			printf("SERVER: \033[0;32mAUTHENTICATION SUCCESSFUL for %s\033[0m\n",inet_ntoa(cli_addr.sin_addr));
+		
+		
+		
+		
 		do{	//Chat Loop
 		if((r=read(connfd,buff,128))<0)
-			printf("SERVER ERROR cannot receive message frm clinet");
-		else
+			printf("SERVER ERROR cannot receive message frm clinet\n");
+		else 
 		{
 			buff[r]='\0';
-
+			
 			if(strcmp(buff,"STOP")==0) {
-				printf("Stop command received, Terminating chat...\n");
+				printf("Stop command received, Terminating chat...\n");			
 				break;
 			}
-			printf("Client '%s' says : %s\n",inet_ntoa(cli_addr.sin_addr),buff);
-
+			printf("\aClient '%s' says:\033[0;36m %s\033[0m\n",inet_ntoa(cli_addr.sin_addr),buff);
+			
+				
 			printf("Enter your message: ");
-			scanf("%s",rbuff);
-
+			scanf(" %[^\n]s",rbuff);
+			
 			if((w=write(connfd,rbuff,128))<0)
-				printf("SERVER ERROR Cannot send message");
-
+				printf("SERVER ERROR Cannot send message\n");	
+				
 			if(strcmp(rbuff,"STOP")==0) {
-				printf("Stop command received, Terminating chat...\n");
+				printf("Stop command received, Terminating chat...\n");			
 				break;
-			}
+			}		
 		}
-		}while(1);
+		}while(1);	
 
 	}
 }
